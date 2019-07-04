2bb7f86b5ecc17d083d5288240ff0af3016440a0
Andrea Zago <andreacw96@gmail.com>
2019-07-04 17:53:12 +0200

    Template & home

---
+++ layouts/shared/navbar.vue
@@ -0,0 +1,77 @@
+<template>
+  <div>
+    <v-toolbar clipped-left dark color="#363636">
+      <v-toolbar-side-icon
+        class="hidden-md-and-up"
+        @click.stop="drawer = !drawer"
+      ></v-toolbar-side-icon>
+      <v-toolbar-title>
+        <nuxt-link
+          class="navbar-item"
+          to="/"
+          title="Vai alla pagina principale"
+        >
+          <span class="white--text">
+            Andrea Zago
+          </span>
+        </nuxt-link>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <template v-for="(nav, i) in navigation">
+        <v-toolbar-items :key="i" class="hidden-sm-and-down" :title="nav.desc">
+          <v-btn flat nuxt :to="nav.link">
+            {{ nav.title }}
+          </v-btn>
+        </v-toolbar-items>
+      </template>
+    </v-toolbar>
+
+    <v-navigation-drawer
+      id="drawer"
+      v-model="drawer"
+      temporary
+      absolute
+      width="200"
+    >
+      <v-list>
+        <v-list-tile
+          v-for="(nav, i) in navigation"
+          :key="i"
+          nuxt
+          :to="nav.link"
+        >
+          <v-list-tile-content>
+            <v-list-tile-title>{{ nav.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hi",
+      drawer: null,
+      navigation: [
+        {
+          title: "Home",
+          link: "/",
+          desc: ""
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style type="text/scss" scoped>
+a.navbar-item {
+  text-decoration: none;
+  &:hover {
+    text-decoration: none;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,74 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <div class="text-xs-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a href="https://vuetifyjs.com" target="_blank">documentation</a>.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat"
-              >discord</a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-              >issue board</a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a href="https://nuxtjs.org/" target="_blank">Nuxt Documentation</a>
-          <br />
-          <a href="https://github.com/nuxt/nuxt.js" target="_blank"
-            >Nuxt GitHub</a
-          >
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" flat nuxt to="/inspire">
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 md12 lg12>
+        <v-card>
+          <v-img
+            src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+            aspect-ratio="2.75"
+          ></v-img>
+
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+              <div>kjl</div>
+            </div>
+          </v-card-title>
+        </v-card>
+      </v-flex>
+      <v-flex xs12 md12 lg12>
+        <v-card>
+          <v-card-title class="darken-1">
+            <div>
+              <div class="headline white--text">Andrea Zago</div>
+              <span class="grey--text">Aka andreacw</span>
+            </div>
+          </v-card-title>
+
+          <v-list>
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon>mail</v-icon>
+              </v-list-tile-action>
+
+              <v-list-tile-content>
+                <v-list-tile-title></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-divider inset></v-divider>
+
+            <v-list-tile>
+              <v-list-tile-action>
+                <v-icon>location_on</v-icon>
+              </v-list-tile-action>
+
+              <v-list-tile-content>
+                <v-list-tile-title></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-import VuetifyLogo from "~/components/VuetifyLogo.vue";
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  head: {
+    title: "Home",
+    meta: [
+      // Open Graph
+      { name: "og:title", content: "this.head.title " },
+      { name: "og:description", content: "" },
+      { name: "og:type", content: "website" },
+      {
+        name: "og:url",
+        content: "https://protezionecivile-settimomilanese.it"
+      },
+      { name: "og:image", content: "https://nuxtjs.org/meta_640.png" }
+    ]
   }
 };
 </script>
