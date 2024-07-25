3b827ec1f0c2b68cd4db03b812ab5b10dbef928b
Andrea Tombolato <andreacw96@gmail.com>
2024-07-25 22:25:15 +0200

    Add Route 66 images to the gallery

--- stores/travels.ts
+++ stores/travels.ts
@@ -69,7 +69,144 @@ export const useTravelStore = defineStore('travels', {
                start: '2022-05-13',
                end: '2022-05-30'
             },
-            photos: 1420
+            photos: 1420,
+            gallery: [
+               {
+                  image: 'https://file-harbor.com/api/v1/files/71867ef5e339a93e15f302f7df800961',
+                  alt: 'Chicago - Illinois',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/29e4e66c6ebdd110a3a6a36866d62440',
+                  alt: 'Chicago - Illinois',
+                  cols: 8
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/0a75a186a45dbdd630ca1d53731bf637',
+                  alt: 'Chicago - Illinois',
+                  cols: 8
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/a6dad869e7607c0b9e72b4d42b8ea492',
+                  alt: 'Chicago - Illinois',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/7d61ddebc1d367bdedc2a32f7033c13a',
+                  alt: 'Chicago - Illinois',
+                  cols: 6
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/73b7c98a17804604cfe6682fe85e6038',
+                  alt: 'Chicago - Illinois',
+                  cols: 6
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/834f906a76a9dd362b5214ea7f7a82fd',
+                  alt: 'Chicago - Illinois',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/78ae14c1ab7debc9ad0192e341a6a86f',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/5e6a00a39ec1a113399156728fd84dcc',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/6bfb9ed7beb0454534044f4c21f87d1a',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/69770f8106b1be9870f8a6d6e7cb9486',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/d403867d8d1cd20a391d23d8fd70d0bb',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/c60ddf932af99c80682ce54ae16d2470',
+                  alt: 'Unknown - Unknown',
+                  cols: 6
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/9f940245e43f4132ef74ea1e3122aeb3',
+                  alt: 'Unknown - Unknown',
+                  cols: 6
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/bf18223cbcc3f2ce57b60e31b922e1e2',
+                  alt: 'Saint Luis - Unknown',
+                  cols: 8
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/5b822aa4a74057293aac67e00ed67367',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/84650a4512ef7f9349e3d1d55b8ec9ad',
+                  alt: 'Unknown - Unknown',
+                  cols: 3
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/8d87e534ae6ebd2a4bca0536fd1e2d6f',
+                  alt: 'Unknown - Unknown',
+                  cols: 5
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/6e0b768d3bd83a2ef0b807e30cba484e',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/7df7045497dd57d5707797f542912936',
+                  alt: 'Unknown - Unknown',
+                  cols: 8
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/a7313d944c8820ea12bfc37dfb03e496',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/e19ed5844bc486e80b4b09973f98071e',
+                  alt: 'Unknown - Unknown',
+                  cols: 8
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/f6d23719e5de7c0c299e5f500a54f9df',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/dc14fc0904abe501f2d1bf520b5efbd8',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/9f6993931bc4afd71f4edeb95689608a',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/fa82a2a2f57b12db6bbcdb2cd57153e4',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+               {
+                  image: 'https://file-harbor.com/api/v1/files/6321a3e4f483f90074d44467babb55d6',
+                  alt: 'Unknown - Unknown',
+                  cols: 4
+               },
+            ]
          },
          {
             code: 'lisbon-2019',
